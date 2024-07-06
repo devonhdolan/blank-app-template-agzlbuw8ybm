a4a9a35c76a678105f60b6f81b5bbf8bc380fcd7
devonhdolan <47202943+devonhdolan@users.noreply.github.com>
2024-07-06 11:51:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import random
+import time
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# List of books and their scenes
+books = {
+    "Alice in Wonderland": [
+        "Alice was beginning to get very tired of sitting by her sister on the bank.",
+        "So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid).",
+        "Whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies."
+    ],
+    "Moby Dick": [
+        "Call me Ishmael. Some years ago - never mind how long precisely - having little or no money in my purse.",
+        "I thought I would sail about
